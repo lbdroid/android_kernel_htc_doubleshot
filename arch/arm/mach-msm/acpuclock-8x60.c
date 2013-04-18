6dc79134e90ee6f1875e268d1b2eae247b17cc8b
Emmanuel Utomi <emmanuelutomi@gmail.com>
2013-04-18 08:22:12 -0500

    Size Optimizations
    Build more drivers as modules to make way for the new size changes in the 4.7.3 toolchain
    
    Also increase low clock voltage to avoid black screens of death
    
    New toolchain here: https://bitbucket.org/codefirex/prebuilts_gcc_linux-x86_arm_arm-eabi-4.7

--- arch/arm/mach-msm/acpuclock-8x60.c
+++ arch/arm/mach-msm/acpuclock-8x60.c
@@ -393,7 +393,7 @@ static struct clkctl_acpu_speed acpu_freq_tbl_nom[] = {
 /* SCPLL frequencies = 2 * 27 MHz * L_VAL */
 #ifdef CONFIG_MACH_DOUBLESHOT
 static struct clkctl_acpu_speed acpu_freq_tbl_fast[] = {
-  { {1, 1},  96000,  ACPU_PLL_8, 3, 1, 0, 0,    L2(1),   400000, 0x03006000},
+  { {1, 1},  96000,  ACPU_PLL_8, 3, 1, 0, 0,    L2(1),   725000, 0x03006000},
   { {1, 1},  128000,  ACPU_PLL_8, 3, 1, 0, 0,    L2(1),   750000, 0x03006000},
   { {1, 1},  153600,  ACPU_PLL_8, 3, 1, 0, 0,    L2(1),   750000, 0x03006000},
   { {1, 1},  192000,  ACPU_PLL_8, 3, 0, 0, 0,    L2(1),   775000, 0x03006000},
