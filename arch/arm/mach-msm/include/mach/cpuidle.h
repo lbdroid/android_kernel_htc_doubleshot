f74de7b3b572e0fd37aa303f404566f0d72fc426
Emmanuel Utomi <emmanuelutomi@gmail.com>
2013-08-17 22:21:13 -0500

    mach/cpuidle.h: Revert back to 3.4.x version

--- arch/arm/mach-msm/include/mach/cpuidle.h
+++ arch/arm/mach-msm/include/mach/cpuidle.h
@@ -32,16 +32,9 @@ static inline s32 msm_cpuidle_get_deep_idle_latency(void) { return 0; }
 #endif
 
 #ifdef CONFIG_CPU_IDLE
-void msm_cpuidle_set_states(struct msm_cpuidle_state *states,
-	int nr_states, struct msm_pm_platform_data *pm_data);
-
 int msm_cpuidle_init(void);
 #else
-static inline void msm_cpuidle_set_states(struct msm_cpuidle_state *states,
-	int nr_states, struct msm_pm_platform_data *pm_data) {}
-
-static inline int msm_cpuidle_init(void)
-{ return -ENOSYS; }
+static inline int msm_cpuidle_init(void) { return -ENOSYS; }
 #endif
 
 #ifdef CONFIG_MSM_SLEEP_STATS
@@ -63,4 +56,4 @@ static inline int msm_cpuidle_unregister_notifier(unsigned int cpu,
 { return -ENODEV; }
 #endif
 
-#endif /* __ARCH_ARM_MACH_MSM_CPUIDLE_H */
+#endif 
